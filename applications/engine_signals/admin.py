f5661b656bf7bbce6d44af7031dbf18577bc7488
hurbx <hugo.urbina@mayor.cl>
2023-03-07 15:27:35 -0300

    admin register - battery models - btms models - bus - electric models - engine - lenze - other - status - technical

--- applications/engine_signals/admin.py
+++ applications/engine_signals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import PositiveTorque, NegativeTorque, EngineTemperature
 # Register your models here.
+
+
+admin.site.register(PositiveTorque)
+admin.site.register(NegativeTorque)
+admin.site.register(EngineTemperature)
